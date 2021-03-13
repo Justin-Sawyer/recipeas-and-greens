b8fd762ec38e98af714844c205084db4dc3510b4
Justin-Sawyer <justin.sawyer@icloud.com>
2021-03-13 20:05:38 +0000

    Add Materialize Navbar and Sidenav

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+/* From Materialize (Mobile collapse Button) https://materializecss.com/navbar.html 
+and from Code Institute Backend Development Mini Project */
+
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"});
+  });
