c000df36a55697c3d39725a594828034d1dafc28
Justin-Sawyer <justin.sawyer@icloud.com>
2021-03-17 18:57:58 +0000

    Add categories dropdown

--- app.py
+++ app.py
@@ -119,7 +119,8 @@ def logout():
 
 @app.route("/add_recipe")
 def add_recipe():
-    return render_template("add_recipe.html")
+    categories = mongo.db.categories.find().sort("recipe_category", 1)
+    return render_template("add_recipe.html", categories=categories)
 
 
 if __name__ == "__main__":
