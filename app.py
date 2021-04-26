401a9b6d0ca22f1d6e282f01f60cc78b047dbab0
Justin-Sawyer <justin.sawyer@icloud.com>
2021-04-26 16:25:06 +0000

    caught error for favourites on recipe page

--- app.py
+++ app.py
@@ -675,4 +675,4 @@ def special_exception_handler(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
